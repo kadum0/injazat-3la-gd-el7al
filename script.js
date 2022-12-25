4e2c5dbbd0f0b7b1ca4e92cc958f04b1a6c73337
kadum <75564464+kadum2@users.noreply.github.com>
2022-12-26 01:17:25 +0300

    translate and some mroe

--- script.js
+++ script.js
@@ -27,7 +27,27 @@ const bygreenConfig = {
 const bygreen = initializeApp(bygreenConfig, 'bygreen');
 const bygreenDb = getFirestore(bygreen)
 
-/////////auth state 
+
+// ui-js
+document.querySelector('#translateToEn').addEventListener('click', (ev)=>{
+    ev.target.classList.toggle('on')
+    if(ev.target.classList.contains('on')){
+        document.querySelectorAll('.en').forEach((enElement)=>enElement.style.display='block')
+        document.querySelectorAll('.ar').forEach((arELement)=>arELement.style.display='none')
+        ev.target.textContent = 'ar'
+    }else{
+        document.querySelectorAll('.en').forEach((enElement)=>enElement.style.display='none')
+        document.querySelectorAll('.ar').forEach((arELement)=>arELement.style.display='block')
+        ev.target.textContent = 'en'
+    }
+})
+
+
+
+
+
+
+
 
 // getting; 
 
