375600684d38145f405845ee360d8ba2eec260b4
kadum <75564464+kadum2@users.noreply.github.com>
2023-01-02 02:51:38 +0300

    fix some

--- script.js
+++ script.js
@@ -1,33 +1,4 @@
 
-
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-app.js";
-
-import {getAuth, createUserWithEmailAndPassword, onAuthStateChanged, signOut, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider} from "https://www.gstatic.com/firebasejs/9.9.2/firebase-auth.js"
-
-import { getFirestore, onSnapshot,
-	collection, doc, getDocs, getDoc,
-	addDoc, deleteDoc, setDoc,
-	query, where, orderBy, serverTimestamp,
-	updateDoc, arrayUnion, arrayRemove} from "https://www.gstatic.com/firebasejs/9.9.2/firebase-firestore.js";
-
-// firebase storage; 
-import {getStorage, ref, uploadBytes, getDownloadURL, listAll, list} from 'https://www.gstatic.com/firebasejs/9.9.2/firebase-storage.js'
-    
-
-const bygreenConfig = {
-    apiKey: "AIzaSyDqK1z4fd7lO9g2ISbf-NNROMd7xpxcahc",
-    authDomain: "bygreen-453c9.firebaseapp.com",
-    projectId: "bygreen-453c9",
-    storageBucket: "bygreen-453c9.appspot.com",
-    messagingSenderId: "19954598250",
-    appId: "1:19954598250:web:ba57c792bdf65dbc18a513",
-    measurementId: "G-265TN8HGKX"};
-
-const bygreen = initializeApp(bygreenConfig, 'bygreen');
-const bygreenDb = getFirestore(bygreen)
-
-
 // ui-js
 document.querySelector('#translateToEn').addEventListener('click', (ev)=>{
     ev.target.classList.toggle('on')
@@ -42,28 +13,5 @@ document.querySelector('#translateToEn').addEventListener('click', (ev)=>{
     }
 })
 
-
-
-
-
-
-
-
-// getting; 
-
-    // getDocs(collection(bygreenDb, 'users')).then((data)=>{
-    //     let docs = []
-    //         data.docs.forEach(doc=>{
-    //             docs.push({...doc.data(), id: doc.id})
-    //         })
-    //         accountsList = docs
-    //         console.log(docs)
-    //         document.querySelector('#accountsCounter').textContent = accountsList.length
-    //         if(accountsList){
-    //             // console.log(accountsList, dbUser)
-    //             ranking('total', 'de')
-    //         }
-
-    //     })
 
 
